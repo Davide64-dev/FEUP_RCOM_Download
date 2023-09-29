28b0424f8afc74756ad308018757932925480216
Davide64-dev <davidpcu@hotmail.com>
2023-09-29 15:00:47 +0100

    small changes to the state machine

--- Lab1-2/statemachine.c
+++ Lab1-2/statemachine.c
@@ -12,51 +12,47 @@ void transition(state_machine* st, unsigned char trans){
             if (trans == FLAG){
                 st->current_state = FLAG_RCV;
                 A = trans;
+                return A;
             }
-            break;
+            return -1;
         case FLAG_RCV:
-            if (trans == FLAG) break;
+            if (trans == FLAG) return -1;
             if (trans == st->adressByte){
                 st->current_state = A_RCV;
                 C = trans;
-                break;
+                return C;
             }
             st->current_state = START;
-            break;
+            return -1;
         case A_RCV:
             if (trans == FLAG){
                 st->current_state = FLAG_RCV;
-                break;
             }
             if (trans == 10){ // não está correto, alterar quando perceber o significado daquela transição
                 st->current_state = C_RCV;
-                break;
             }
             st->current_state = START;
-            break;
+            return -1;
 
         case C_RCV:
             if (trans == FLAG){
                 st->current_state = START;
-                break;
             }
             else if (trans == A^C){
                 st->current_state = BCC_OK;
-                break;
             }
             st->current_state = START;
-            break;
+            return -1;
         
         case BCC_OK:
           if (trans == FLAG){
               st->current_state = STOP;
-              break;
           }
           st->current_state = START;
-          break;
+          return -1;
 
         default:
-            break;
+            return -1;
 
     }
 }
