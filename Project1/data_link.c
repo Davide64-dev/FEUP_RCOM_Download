e0398a8a571b62ed151096d0080a3b0e7f591b85
Davide64-dev <davidpcu@hotmail.com>
2023-10-19 09:58:06 +0100

    the llOpenTransmiter already has an implemented state machine

--- Project1/data_link.c
+++ Project1/data_link.c
@@ -10,23 +10,21 @@ int llOpenTransmiter(struct linkLayer* ll){
     unsigned char buf[SET_SIZE] = {FLAG, A_SENDER, C_SET, A_SENDER ^ C_SET, FLAG};
     int finish = FALSE;
 
+    state_machine* st = (state_machine*)malloc(sizeof(state_machine));
+
     while(finish == FALSE){
+        st->current_state = START;
         int bytes = write(fd, buf, SET_SIZE);
         int bytes1 = read(fd, buf, SET_SIZE);
-        buf[bytes1] = '\0';
-        if (buf[0] == 'z')
-            STOP = TRUE;
-        alarm(3);
-
-        if (bytes1 >= 0){
-            alarm(0);
+        transition(st, buf, bytes1, A_RECEIVER, C_UA);
+        if (st->current_state == STATE_STOP){
             finish = TRUE;
             printf("%d\n", bytes);
             printf("Transmiter Opened with success\n");
-            return bytes;
+            return 0;
         }
     }
-    return 0;
+    return -1;
 }
 
 int llOpenReceiver(struct linkLayer* ll){
@@ -34,10 +32,8 @@ int llOpenReceiver(struct linkLayer* ll){
     unsigned char buf[SET_SIZE];
 
     state_machine* st = (state_machine*)malloc(sizeof(state_machine));
-    st->adressByte = A_SENDER;
 
-    while (STOP == FALSE)
-    {
+    while (STOP == FALSE){
         st->current_state = START;
         int bytes = read(fd, buf, SET_SIZE);
         buf[bytes] = '\0';
@@ -46,9 +42,6 @@ int llOpenReceiver(struct linkLayer* ll){
             unsigned char buf[SET_SIZE] = {FLAG, A_RECEIVER, C_UA, A_RECEIVER ^ C_UA, FLAG};
             write(fd, buf, SET_SIZE);
             STOP = TRUE;
-        }
-        else{
-            printf("wrong package\n");
         }
     }
 
@@ -61,10 +54,7 @@ int llopen(struct linkLayer* ll, int mode){
     if (mode == TRANSMITER){
         return llOpenTransmiter(ll);
     }
-    else if (mode == RECEIVER){
-        return llOpenReceiver(ll);
-    }
-    return 0;
+    return llOpenReceiver(ll);
 }
 
 unsigned char BCC2(unsigned char* frame, int len){
