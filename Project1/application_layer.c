dcb269c8ce51e9c09c95ebd785683dd0c891e148
unknown <up202108849@up.pt>
2023-11-10 14:51:59 +0000

    Changed variables

--- Project1/application_layer.c
+++ Project1/application_layer.c
@@ -2,27 +2,27 @@
 
 
 unsigned char* createControlPacket(int C, long FileSize, const char* filename, int* packetSize){
-    const int L1 = sizeof(FileSize);
-    const int L2 = strlen(filename);
-    int finalSize = 1 + 2 + L1 + 2 + L2;
+    const int LenL1 = sizeof(FileSize);
+    const int LenL2 = strlen(filename);
+    int finalSize =  LenL1 + LenL2 + 5;
     *packetSize = finalSize;
 
     unsigned char* ret = (unsigned char*)malloc(finalSize);
 
     ret[0] = C;
     ret[1] = 0;
-    ret[2] = L1;
+    ret[2] = LenL1;
 
-    for (unsigned char i = 0 ; i < L1 ; i++) {
-        ret[2+L1-i] = FileSize | 0x00;
+    for (unsigned char i = 0 ; i < LenL1 ; i++) {
+        ret[2+LenL1-i] = FileSize | 0x00;
         FileSize >>= 8;
     }
 
-    ret[L1+3] = 1;
-    ret[L1+4] = L2;
+    ret[LenL1+3] = 1;
+    ret[LenL1+4] = LenL2;
 
-    for (int i = 0; i < L2; i++){
-        ret[L1 + 5 + i] = filename[i];
+    for (int i = 0; i < LenL2; i++){
+        ret[LenL1 + 5 + i] = filename[i];
     }
 
     return ret;
@@ -44,14 +44,14 @@ unsigned char* decodeControlPacket(unsigned char* packet, int size, unsigned lon
 }
 
 unsigned char* createDataPacket(const unsigned char* packageContent, int packageContentSize, int* packetSize){
-    int L2 = packageContentSize / 256;
-    int L1 = packageContentSize % 256;
+    int LenL2 = packageContentSize / 256;
+    int LenL1 = packageContentSize % 256;
     *packetSize = 3 + packageContentSize;
     unsigned char* ret = (unsigned char*)malloc(*packetSize);
 
     ret[0] = 1;
-    ret[1] = L2;
-    ret[2] = L1;
+    ret[1] = LenL2;
+    ret[2] = LenL1;
     for (int i = 0; i < packageContentSize; i++){
         ret[i + 3] = packageContent[i];
     }
